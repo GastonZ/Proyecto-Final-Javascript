4aa836d09ad620f4018143f668de4f27de3a545b
GastonZ <gastonzappullaryu@gmail.com>
2022-08-12 15:13:26 -0300

    Tercer commit agregando local storage a merch

---
+++ paginas/stock.js
@@ -0,0 +1,5 @@
+let stockPlanes = [
+    {id: 1, nombre: "Fitness Plan", cantidad: 1, precio: 1499},
+    {id: 1, nombre: "Fitness Plan", cantidad: 1, precio: 1499},
+    {id: 1, nombre: "Fitness Plan", cantidad: 1, precio: 1499}
+]
